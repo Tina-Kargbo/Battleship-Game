02b493abb1aab67b602e62e8e950798bf2d2dd4b
Tina-Kargbo <cl.amankwaah@gmail.com>
2022-12-26 21:29:31 +0000

    out line board

--- run.py
+++ run.py
@@ -6,10 +6,18 @@ print(username)
 
 class Board:
     """User board with grids where the battleship game can be played."""
+    def __init__(self, board):
+        self.board = board
 
-    def __init__(self, size, ships_num):
-        self.size = size
-        self.ships = ships_num
+    #Translates the alphabet to the board numbers to help differentiate rows from columns. 
+    def get_alphabet_as_numbers():
+        alphabet_as_numbers {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4}
+        return alphabet_as_numbers
+    
+    def print board(self):
+        print("a, b, c, d, e")
+        row = 1
+        for row in self.board:
+            print()
 
-MyBoard = Board(5, 4,)
-print(self)
+
