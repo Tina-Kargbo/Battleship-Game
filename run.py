768772fe1881d11de82fdc81c93842923d515b76
Tina-Kargbo <cl.amankwaah@gmail.com>
2022-12-26 16:23:23 +0000

    Add n userame input

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 import random
 
-class MyBoard:
+    #code referenced from CI lesson on "What are Input Operations?
+username = input("Please Type in your Name: ")
+print(username)
+
+class Board:
     """User board with grids where the battleship game can be played."""
-    def __init__(self, size, ships):
 
+    def __init__(self, size, ships_num):
+        self.size = size
+        self.ships = ships_num
 
-class ComputerBoard:
-    """Computer board with grids where the battleship game can be played."""
-    def __init__(self, size, ships):
+MyBoard = Board(5, 4,)
+print(self)
