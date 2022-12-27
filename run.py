3061c63951224eebc02a1c084acc1414ec08a719
Tina-Kargbo <cl.amankwaah@gmail.com>
2022-12-27 19:04:56 +0000

    add define column and row, print commands

--- run.py
+++ run.py
@@ -3,10 +3,20 @@ from random import randint
 board = []
 
 for x in range(4):
-    board.append([O] * 4)
+    board.append(["O"] *4)
 
 def print_board(board):
     for row in board:
-        print " ".join(row)
+        print_board(board)
 
-print_board(board)
+def random_row(board):
+    return randint(0, len(board[0]) - 1)
+
+def random_column(board):
+    return randint(0, len(board[0]) - 1)
+
+grid_row = random_row(board)
+grid_column = random_column(board)
+
+print(grid_row)
+print(grid_column)
